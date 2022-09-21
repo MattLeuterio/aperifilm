acc36b907258d54a9187b351e7fcdd803bd073ed
Matt Leuterio <leuterio.matteo@gmail.com>
2022-09-21 12:09:58 +0200

    howItWorks and AboutUs pages

---
+++ src/styles/Pages/aboutUsStyle.js
@@ -0,0 +1,8 @@
+import styled, { css } from 'styled-components';
+import { sidebarWidthDSK } from '../../../const';
+import { DisplayFlex, LineClamp } from '../../js/Mixins';
+import { imgBasePath } from '../../js/utility';
+import theme from '../../theme';
+
+export const AboutUsContainer = styled.div`
+`;
